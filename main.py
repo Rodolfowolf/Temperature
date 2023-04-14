38ddde318efb9a697839e43d21defa0db1bcd301
Rodolfo Silva <126601333+Rodolfowolf@users.noreply.github.com>
2023-04-14 10:27:51 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+temperatura = int(input("Digite a temperatura: "))
+                  
+
+if temperatura > 71:
+   print("Well done")
+elif temperatura > 65:
+   print("Medium well")
+elif temperatura > 60:
+   print("Medium")
+elif temperatura > 54:
+   print("Medium rare")
+elif temperatura > 48:
+   print("Rare")
+else:
+   print("Cozimento")
